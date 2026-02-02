bafa382d4f052fbb61609a58350b755b168011d4
Raul5304 <raullecinena@iesrioarba.es>
2026-02-02 13:13:20 +0100

    Commit javascript nose

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const titulo = document.querySelector("h1");
+
+    titulo.addEventListener("click", () => {
+        titulo.textContent = "Has hecho clic en el título";
+        titulo.style.color = "blue";
+    });
+});
